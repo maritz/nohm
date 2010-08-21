ba751b46a12b03c82de257819f87fff546e9d87b
Moritz Peters <moritz@mpeters.biz>
2010-08-21 17:31:00 +0200

    Updated nodeunit Added a few tests Fixed this.relationchanges on instanciation

--- test/relations.js
+++ test/relations.js
@@ -54,8 +54,26 @@ var RoleLinkMockup = nohm.Model.extend({
         value: 'user'
       }
     };
+    nohm.Model.call(this);
   }
 });
+
+exports.instances = function (t) {
+  var user = new UserLinkMockup(),
+  role = new RoleLinkMockup(),
+  role2;
+
+  t.expect(2);
+
+  role.link(user);
+
+  t.ok(role.relationChanges !== user.relationChanges, 'Instances share the relationchanges, initiate them as an empty array in the constructor.');
+
+  role2 = new RoleLinkMockup();
+  t.same(role2.relationChanges, [], 'Creating a new instance does not reset the relationchanges of that instance.');
+
+  t.done();
+};
 
 
 exports.link = function (t) {
@@ -63,25 +81,25 @@ exports.link = function (t) {
   role = new RoleLinkMockup(),
   linkCallbackCalled = false;
   t.expect(10);
-  
+
   user.link(role, function (action, on, name, obj) {
     linkCallbackCalled = true;
-    t.ok(action === 'link', 'The argument "action" given to the link callback are not correct');
-    t.ok(on === 'UserLinkMockup', 'The argument "on" given to the link callback are not correct');
-    t.ok(name === 'child', 'The argument "name" given to the link callback are not correct');
-    t.ok(obj === role, 'The argument "obj" given to the link callback are not correct');
+    t.equals(action, 'link', 'The argument "action" given to the link callback are not correct');
+    t.equals(on, 'UserLinkMockup', 'The argument "on" given to the link callback are not correct');
+    t.equals(name, 'child', 'The argument "name" given to the link callback are not correct');
+    t.same(obj, role, 'The argument "obj" given to the link callback are not correct');
   });
-  
+
   user.save(function (err) {
     if (!err) {
       t.ok(linkCallbackCalled, 'The provided callback for linking was not called.');
-      redis.keys('*:relations:*', function (err, values) {
+      redis.keys(prefix + '*:relations:*', function (err, values) {
         var args = [],
         key,
         firstDone = false,
         keyCheck = function (err, members) {
-          t.ok(members.length === 1, 'The set of a relationship does not have exactly one relationship entry');
-          t.ok(members[0].toString() === '1', 'The set of a relationship contained a wrong member');
+          t.equals(members.length, 1, 'The set of a relationship does not have exactly one relationship entry');
+          t.equals(members[0], '1', 'The set of a relationship contained a wrong member');
           if (firstDone === true) {
             t.done();
           } else {
@@ -107,24 +125,24 @@ exports.unlink = function (t) {
   role = new RoleLinkMockup(),
   unlinkCallbackCalled = false;
   t.expect(6);
-  
+
   user.id = 1;
   role.id = 1;
-  
+
   user.unlink(role, function (action, on, name, obj) {
     unlinkCallbackCalled = true;
-    t.ok(action === 'unlink', 'The argument "action" given to the unlink callback are not correct');
-    t.ok(on === 'UserLinkMockup', 'The argument "on" given to the unlink callback are not correct');
-    t.ok(name === 'child', 'The argument "name" given to the unlink callback are not correct');
-    t.ok(obj === role, 'The argument "obj" given to the unlink callback are not correct');
+    t.equals(action, 'unlink', 'The argument "action" given to the unlink callback are not correct');
+    t.equals(on, 'UserLinkMockup', 'The argument "on" given to the unlink callback are not correct');
+    t.equals(name, 'child', 'The argument "name" given to the unlink callback are not correct');
+    t.equals(obj, role, 'The argument "obj" given to the unlink callback are not correct');
   });
-  
+
   user.save(function (err) {
     if (!err) {
       t.ok(unlinkCallbackCalled, 'The provided callback for unlinking was not called.');
       redis.keys('*:relations:*', function (err, values) {
         if (!err) {
-          t.ok(values === null, 'Unlinking an object did not delete keys.');
+          t.equals(values, null, 'Unlinking an object did not delete keys.');
         }
         t.done();
       });
@@ -132,5 +150,32 @@ exports.unlink = function (t) {
       console.dir(err);
       t.done();
     }
+  });
+};
+
+exports.deeplink = function (t) {
+  var user = new UserLinkMockup(),
+  role = new RoleLinkMockup(),
+  comment = new CommentLinkMockup(),
+  userLinkCallbackCalled = false,
+  commentLinkCallbackCalled = false;
+  t.expect(3);
+
+  role.link(user, function (action, on, name, obj) {
+    userLinkCallbackCalled = true;
+  });
+  user.link(comment, function (action, on, name, obj) {
+    commentLinkCallbackCalled = true;
+  });
+
+  role.save(function (err) {
+    if (!err) {
+      t.ok(userLinkCallbackCalled, 'The user link callback was not called.');
+      t.ok(commentLinkCallbackCalled, 'The comment link callback was not called.');
+      t.ok(comment.id !== null, 'The deeplinked comment does not have an id and thus is probably not saved correctly.');
+    } else {
+      console.dir(err);
+    }
+    t.done();
   });
 };
