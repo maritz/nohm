6a96bd4476ef1782a449d1a1d7ca330b4c4d7d05
Pier Paolo Ramon <ramonpierre@gmail.com>
2011-12-12 14:39:17 +0100

    Remvoe poorly executed silent mode, even better tests.
    Also `.save` and `.remove` can be called without a callback, and accept
    an option hash to pass `silent: true`. Can be used for future
    enhancements.

--- test/pubsubTests.js
+++ test/pubsubTests.js
@@ -10,48 +10,160 @@ var secondaryClient = redis.createClient();
 nohm.setPubSubClient(secondaryClient);
 
 var Tester = nohm.model('Tester', {
-	properties: {
-		dummy: {
-			type: 'string'
-		}
-	}
+  properties: {
+    dummy: {
+      type: 'string'
+    }
+  }
 });
 
 // TODO base pub/sub tests.
 
 var after = function (times, fn) {
-	return function () {
-		if ((times--)<=1) {
-			fn.apply(this, arguments);
-		}
-	};
+  return function () {
+    if ((times--)==1) {
+      fn.apply(this, arguments);
+    }
+  };
 };
+
+exports.afterLimiter = function(t) {
+
+  var counter = 0;
+
+  var _test = after(3, function () {
+    counter += 1;
+  });
+
+  _test(); _test(); _test(); _test();
+
+  t.equal(counter, 1, 'Function has been called a wrong number of times');
+  t.done();
+
+};
+
+var obj;
 
 exports.creationOnce = function(t) {
 
-	var obj;
-	t.expect(4);
+  t.expect(5);
 
-	var _done = after(2, function () {
-		t.done();
-	});
+  var _done = after(3, function () {
+    t.done();
+  });
 
-	Tester.subscribeOnce('create', function (payload) {
-		t.ok(!!payload, 'Subscriber did not recieved a payload.');
-		t.ok(!!payload.target, 'Subscriber did not recieved a payload.target object.');
-		t.equal(payload.target.id, obj.id, 'Passed payload has an incorrect ID.');
-		_done();
-	});
+  Tester.subscribeOnce('create', function (payload) {
 
-	obj = new Tester();
-	obj.p({
-		dummy: 'some string'
-	});
+    var expectedPayload = {
+      target: {
+        id: obj.id,
+        properties: {
+          dummy: 'some string',
+          id: obj.id
+        }
+      }
+    };
 
-	obj.save(function(err){
-		t.ok(!err, 'There was an error during .save.');
-		_done();
-	})
+    t.ok(!!payload, 'Subscriber did not recieved a payload.');
+    t.ok(!!payload.target, 'Subscriber did not recieved a payload.target object.');
+    t.equal(payload.target.id, obj.id, 'Passed payload has an incorrect ID.');
 
-}
+    t.deepEqual(payload, expectedPayload, "Passed payload is not equal to expected one.");
+
+    _done();
+  });
+
+  obj = new Tester();
+  obj.p({
+    dummy: 'some string'
+  });
+
+  obj.save(function(err){
+    t.ok(!err, 'There was an error during .save.');
+    _done();
+  });
+
+  _done();
+
+};
+
+exports.removeOnce = function (t) {
+
+  var _id = obj.id;
+
+  t.expect(5);
+
+  var _done = after(3, function () {
+    t.done();
+  });
+
+  Tester.subscribe('remove', function (payload) {
+
+    var expectedPayload = {
+      target: {
+        id: _id,
+        properties: {
+          id: 0,
+          dummy: 'some string'
+        }
+      }
+    };
+
+    t.ok(!!payload, 'Subscriber did not recieved a payload');
+    t.ok(!!payload.target, 'Subscriber did not recieved a payload.target object.');
+    t.equal(payload.target.id, _id, 'Passed payload has an incorrect ID.')
+
+    t.deepEqual(payload, expectedPayload, "Passed payload is not equal to expected one.");
+
+    _done();
+  });
+
+  obj.remove(function(err){
+    t.ok(!err, 'There was an error during .remove.');
+    _done();
+  });
+
+  _done();
+
+};
+
+exports.silencedCreation = function (t) {
+
+  t.expect(0);
+
+  var _done = after(2, function () {
+    t.done();
+  });
+
+  var run = 0;
+
+  obj = new Tester();
+
+  obj.p({ dummy: 'some strings' });
+
+  Tester.subscribe('create', function (payload) {
+    var msg = run == 1 ? 'first run' : 'second run, listener not unsubscribed';
+    t.ok(false, 'The subscriber has run, during silenced actions ('+msg+')');
+  });
+
+  run = 1;
+
+  obj.save({ silent: true }, function () {
+    _done();
+  });
+
+  setTimeout(function(){
+
+    Tester.unsubscribe('create');
+
+    run = 2;
+    obj.p({ dummy: 'some other string' });
+
+    obj.save({ silent: true }, function () {
+      _done();
+    });
+
+  }, 500);
+
+};
 
